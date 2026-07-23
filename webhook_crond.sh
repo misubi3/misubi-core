b19fc1f057cc819abd6745b57c18ba143a9ae8b0
misubi3 <mehakali9873@gmail.com>
2026-07-24 00:23:11 +0530

    Delete webhook_crond.sh

--- webhook_crond.sh
+++
@@ -1,5 +0,0 @@
-{
-  "env_image_name": "nextjs_mongo_shadcn_base_image_cloud_arm:release-26062026-1",
-  "job_id": "c8d0226a-20c9-4d08-a832-33fae26c989a",
-  "created_at": "2026-07-19T14:32:08.730116+00:00Z"
-}
